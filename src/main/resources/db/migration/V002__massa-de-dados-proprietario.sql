3d6a7d576d8138bd08e4402cc3830a41031e287c
guntz <rricardoguntzell@gmail.com>
2024-11-22 20:29:46 -0300

    feat: inclusão de massa de dados para proprietario

---
+++ src/main/resources/db/migration/V002__massa-de-dados-proprietario.sql
@@ -0,0 +1,3 @@
+INSERT INTO tb_proprietario (nome, email, telefone, situacao) VALUES ('Neelix', 'neelix@offbeat.com', '11988887777', 'ATIVO')
+,('Phaxe', 'phaxe@offbeat.com', '11988886666', 'ATIVO')
+,('Ranji', 'ranji@offbeat.com', '11988885555', 'ATIVO');
